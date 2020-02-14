96ef28f305105e26a87f283774318193b0a27a38
Yuna Son <reisei88.dev@gmail.com>
2020-02-14 23:28:39 +0900

    200214, Python list, 관련 메소드, 슬라이스와 메소드 차이, 객체

---
+++ Python/panic2.py
@@ -0,0 +1,11 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print("phrase: ", phrase)
+print("plist: ", plist)
+
+new_phrase = ''.join(plist[1:3])
+new_phrase = ''.join([plist[5],plist[4],plist[7],plist[6]])
+
+new_phrase =''.join(plist)
+print("plist: ",plist)
+print("new_phrase: ",new_phrase)
