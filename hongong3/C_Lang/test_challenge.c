68b7fd9195fe818f07960faa31e7b6aa0354666f
Yuna Son <reisei88.dev@gmail.com>
2020-02-16 19:53:12 +0900

    혼공C 2주차

---
+++ hongong3/C_Lang/test_challenge.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double w, h, hm;
+    double bmi;
+    int a = 1, b = 0, res;
+
+    printf("몸무게(kg)와 키(cm)를 입력: ");
+    scanf("%lf%lf", &w, &h);
+    printf("몸무게 %lf, 키 %lf", w, h);
+ 
+    hm = h/100;
+
+    bmi = (w/(hm*hm));
+    printf("bmi지수는: %lf\n", bmi);
+
+    res = (20 <= bmi && bmi < 25) ? a : b;
+    printf("%d\n", res);
+
+    return 0;
+}
